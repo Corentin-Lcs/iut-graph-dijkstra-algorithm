2a3c951f5749d6b1ae05155e00b68c8eda428bf6
Corentin Lenclos <corentin.lenclos@gmail.com>
2023-07-11 14:14:32 +0200

    Initial commit

---
+++ src/main/graphe/exceptions/ArcInexistantException.java
@@ -0,0 +1,19 @@
+package src.main.graphe.exceptions;
+
+import java.io.Serial;
+
+/**
+ * Exception thrown when an arc does not exist, and it is expected to.
+ * @author Corentin LENCLOS / Seweryn CZYKINOWSKI
+ */
+public class ArcInexistantException extends IllegalArgumentException {
+    @Serial
+    private static final long serialVersionUID = 4L;
+
+    /**
+     * Default constructor with predefined message
+     */
+    public ArcInexistantException() {
+        super("Arc inexistant.");
+    }
+}
